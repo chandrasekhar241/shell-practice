41a1fb56884b766cf5bf8bb706cbb8f4d3832794
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-06-27 22:05:18 +0530

    usage of variabkes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,3 +2,9 @@
 
 #this is comment
 echo "Hello world"
+
+PERSON1 = Russia
+PERSON2 = america
+
+echo "$PERSON1 : Hey ukraine we are going to bladt you!"
+echo "$PERSON2 : Yes, please you are welcome"
