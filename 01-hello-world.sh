174b9dd17b5ca1ae65c1e7c6c6d6bdd465e371d0
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-06-27 22:09:35 +0530

    passing values from command line

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,8 +3,8 @@
 #this is comment
 echo "Hello world"
 
-PERSON1 = Russia
-PERSON2 = america
+PERSON1 = $1
+PERSON2 = $2
 
 echo "$PERSON1 : Hey ukraine we are going to bladt you!"
 echo "$PERSON2 : Yes, please you are welcome"
