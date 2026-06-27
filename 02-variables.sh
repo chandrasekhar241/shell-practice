c9ba9e610a163998a768e801f99f7f780e459382
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-06-27 22:27:56 +0530

    read command

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,3 +5,10 @@ echo "please enter your name : :"
 read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in this variable
 
 echo "username is $USER_NAME"
+
+
+echo "please enter your password : :"
+
+read -s PASSWORD # here USER_NAME is variable, whatever you enter in terminal stores in this variable
+
+echo "password is $PASSWORD"
