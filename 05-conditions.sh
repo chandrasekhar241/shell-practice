da56ff375ef36c8be2d81c2297d6fc493f972515
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-06-28 10:39:50 +0530

    conditions

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -6,6 +6,6 @@ NUMBER=$1
 
 if [ $NUMBER -ge 20]; then
     echo "Given number is greater that or equal to 20"
-else
-    echo "Given number is less than 20"
+    else
+        echo "Given number is less than 20"
 fi
