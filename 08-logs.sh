35ea0dee467a317d1c5ef2e82953148c9e113acf
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-06-28 12:50:42 +0530

    using conditions to install mysql

---
+++ 08-logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/home/ec-user/shell-logs
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+#check root acess or not
+if [ $USERID -ne 0 ]; then
+    echo "please run this script using root access"
+    exit 1
+fi
+
+#echo  "I am continued"
+
+#first arg - what are u trying to install
+#second arg - exit code
+VALIDATE(){
+    if  [ $2 -ne 0 ]; then
+        echo "Intsalling $1 is .. failed"
+        exit 1
+    else
+        echo "installed $1 .. sucessfully"
+    fi
+
+}
+
+dnf list installed nginx &>> $LOGS_FILE
+
+if [ $? -eq 0 ]; then
+    echo "ngnix already installed"
+else
+    echo "Installing /ngnix"
+    dnf install nginx -y
+    VALIDATE nginx $?
+fi
