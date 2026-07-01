1706b6c3e873642a13b96022f49f2f05882e9172
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-07-01 20:57:52 +0530

    loops practice

--- 10-loops.sh
+++ 10-loops.sh
@@ -3,6 +3,10 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+R="\e[31m"
+Y="\e[33m"
+G="\e[32m"
+N=R="\e[0m"
 
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
@@ -18,10 +22,10 @@ fi
 #second arg - exit code
 VALIDATE(){
     if  [ $2 -ne 0 ]; then
-        echo "$TIMESTAMP [ERROR] Intsalling $1 is .. failed" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] Intsalling $1 is .. $R failed $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$TIMESTAMP [INFO] installed $1 .. sucessfully" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [INFO] installed $1 .. $G sucessfully $N" | tee -a $LOGS_FILE
     fi
 
 }
@@ -34,7 +38,7 @@ do
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE "Installing $package" $?
     else 
-        echo "$TIMESTAMP  $package already installed... skiping"
+        echo -e "$TIMESTAMP  $package already installed... $Y skiping $N"
     fi
 done
 
