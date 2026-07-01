e9c2059740f4bfd80f80ec9648638dac85922bb6
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-07-01 20:50:31 +0530

    loops practice

--- 10-loops.sh
+++ 10-loops.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
 
-TIMESTAMP=${date "+%Y-%m-%d %H:%M:%S"}
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 #check root acess or not
 if [ $USERID -ne 0 ]; then
