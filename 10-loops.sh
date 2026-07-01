4a9d782d3a25a03b34d82d2a869ddc61fb5cfb14
chandrasekhar241 <chandrasekharthullur@gmail.com>
2026-07-01 20:48:51 +0530

    loops practice

--- 10-loops.sh
+++ 10-loops.sh
@@ -4,6 +4,8 @@ USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
 
+TIMESTAMP=${date "+%Y-%m-%d %H:%M:%S"}
+
 #check root acess or not
 if [ $USERID -ne 0 ]; then
     echo "please run this script using root access"
@@ -16,10 +18,10 @@ fi
 #second arg - exit code
 VALIDATE(){
     if  [ $2 -ne 0 ]; then
-        echo "Intsalling $1 is .. failed" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP [ERROR] Intsalling $1 is .. failed" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "installed $1 .. sucessfully" | tee -a $LOGS_FILE
+        echo "$$TIMESTAMP [INFO] installed $1 .. sucessfully" | tee -a $LOGS_FILE
     fi
 
 }
@@ -27,13 +29,12 @@ VALIDATE(){
 for package in $@
 do
     echo "installing $package"
-    dnf list installed $package
-    if [$? -ne 0 ]; then
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE "Installing $package" $?
     else 
         echo "$package already installed... skiping"
     fi
-
 done
 
